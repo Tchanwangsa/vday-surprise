df0da011ceb02826812bc68b4923c382e43e2561
tc2509617 <tanatpond@gmail.com>
2025-02-07 03:38:50 +0700

    update title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
-import "./globals.css"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import type React from "react" // Import React
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import type React from "react"; // Import React
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "A Special Valentine's Day Surprise",
-  description: "A personalized Valentine's Day experience just for you",
-}
+  title: "Surprise!!!",
+  description: ".......",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
-
